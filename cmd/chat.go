9608bfc4aa0cc0b483b9cb29ff3759323f6b7f9a
vinit-chauhan <chauhanvinit23@gmail.com>
2025-04-18 13:31:58 -0400

    implemented chat command

--- cmd/chat.go
+++ cmd/chat.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/vinit-chauhan/devmind/config"
+	"github.com/vinit-chauhan/devmind/internal/agent"
+	"github.com/vinit-chauhan/devmind/internal/logger"
 )
 
 var chatCmd = &cobra.Command{
@@ -12,13 +16,31 @@ var chatCmd = &cobra.Command{
 	Short: "Chat with the mind",
 	Long:  `Chat with the mind. You can ask it anything and it will try to help you. It is a command line tool that can be used to generate, explain and fix code snippets, complete code, and more.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Chat command executed")
+		logger.Info("Chat command executed")
+
+		backend, err := agent.GetBackend(config.Config.Backend)
+		if err != nil {
+			msg := "Error getting backend: " + err.Error()
+			logger.Error(msg)
+			fmt.Fprintln(os.Stderr, msg)
+			return
+		}
 
 		message := strings.Builder{}
 		for _, arg := range args {
 			message.WriteString(arg + " ")
 		}
-		fmt.Printf("Message: %s\n", message.String())
+		logger.Debug("Message: " + message.String())
+
+		resp, err := backend.Respond(message.String())
+		if err != nil {
+			msg := "Error getting response: " + err.Error()
+			logger.Error(msg)
+			fmt.Fprintln(os.Stderr, msg)
+			return
+		}
+
+		fmt.Printf("Response: %s\n", resp)
 	},
 }
 
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/vinit-chauhan/devmind/cmd"
+	"github.com/vinit-chauhan/devmind/config"
 )
 
 func main() {
+	// Initialize the configuration
+	config.InitConfig()
+
+	// Execute the root command
 	cmd.Execute()
 }
